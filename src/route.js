e68229bce040e66c7c9cc2697f769e2bd9f008a7
Arthu76 <arthurhensilva18@gmail.com>
2021-10-29 16:14:47 -0300

    RocketQ

---
+++ src/route.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render('index'))
+route.get('/room', (req, res) => res.render('room'))
+route.get('/create-pass', (req, res) => res.render('create-pass'))
+
+//Formato que o formulário de dentro da modal tem que passar a informação
+//oute.get('/room/:room/:question/:action')
+
+module.exports = route
